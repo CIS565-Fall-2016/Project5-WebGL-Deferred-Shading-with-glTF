5c43a043b60296b2a0e5c3f7389f8b76e6fff32a
Ethan <ethanabrooks@gmail.com>
2016-11-01 18:18:24 -0400

    about to pull. only uncommented some stuff

--- js/deferredRender.js
+++ js/deferredRender.js
@@ -28,12 +28,12 @@
         // CHECKITOUT: START HERE! You can even uncomment this:
         //debugger;
 
-        { // TODO: this block should be removed after testing renderFullScreenQuad
-            gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-            // TODO: Implement/test renderFullScreenQuad first
-            renderFullScreenQuad(R.progRed);
-            return;
-        }
+        // { // TODO: this block should be removed after testing renderFullScreenQuad
+        //     gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        //     // TODO: Implement/test renderFullScreenQuad first
+        //     renderFullScreenQuad(R.progRed);
+        //     return;
+        // }
 
         R.pass_copy.render(state);
 
@@ -101,17 +101,17 @@
     R.pass_debug.render = function(state) {
         // * Unbind any framebuffer, so we can write to the screen
         // TODO: uncomment
-        // gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 
         // * Bind/setup the debug "lighting" pass
         // * Tell shader which debug view to use
         // TODO: uncomment
-        // bindTexturesForLightPass(R.prog_Debug);
-        // gl.uniform1i(R.prog_Debug.u_debug, cfg.debugView);
+        bindTexturesForLightPass(R.prog_Debug);
+        gl.uniform1i(R.prog_Debug.u_debug, cfg.debugView);
 
         // * Render a fullscreen quad to perform shading on
         // TODO: uncomment
-        // renderFullScreenQuad(R.prog_Debug);
+        renderFullScreenQuad(R.prog_Debug);
     };
 
     /**
@@ -130,7 +130,7 @@
 
         // Enable blending and use gl.blendFunc to blend with:
         //   color = 1 * src_color + 1 * dst_color
-        // Here is a wonderful demo of showing how blend function works: 
+        // Here is a wonderful demo of showing how blend function works:
         // http://mrdoob.github.io/webgl-blendfunctions/blendfunc.html
         // TODO: uncomment
         // gl.enable(gl.BLEND);
@@ -230,12 +230,12 @@
 
             // Bind the VBO as the gl.ARRAY_BUFFER
             // TODO: uncomment
-            // gl.bindBuffer(gl.ARRAY_BUFFER,vbo);
+            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
 
             // Upload the positions array to the currently-bound array buffer
             // using gl.bufferData in static draw mode.
             // TODO: uncomment
-            // gl.bufferData(gl.ARRAY_BUFFER,positions,gl.STATIC_DRAW);
+            gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
         };
 
         return function(prog) {
@@ -249,21 +249,21 @@
 
             // Bind the VBO as the gl.ARRAY_BUFFER
             // TODO: uncomment
-            // gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+            gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
 
             // Enable the bound buffer as the vertex attrib array for
             // prog.a_position, using gl.enableVertexAttribArray
             // TODO: uncomment
-            // gl.enableVertexAttribArray(prog.a_position);
+            gl.enableVertexAttribArray(prog.a_position);
 
             // Use gl.vertexAttribPointer to tell WebGL the type/layout for
             // prog.a_position's access pattern.
             // TODO: uncomment
-            // gl.vertexAttribPointer(prog.a_position, 3, gl.FLOAT, gl.FALSE, 0, 0);
+            gl.vertexAttribPointer(prog.a_position, 3, gl.FLOAT, gl.FALSE, 0, 0);
 
             // Use gl.drawArrays (or gl.drawElements) to draw your quad.
             // TODO: uncomment
-            // gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+            gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
             // Unbind the array buffer.
             gl.bindBuffer(gl.ARRAY_BUFFER, null);
