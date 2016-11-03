5385ec0372c695e41ca041f14ee3ecf39faba8f8
Akshay Shah <shahak@seas.upenn.edu>
2016-11-03 13:52:23 -0400

    something wrong with ambient, maybe

--- glsl/deferred/ambient.frag.glsl
+++ glsl/deferred/ambient.frag.glsl
@@ -10,6 +10,8 @@ uniform sampler2D u_depth;
 
 varying vec2 v_uv;
 
+const vec3 ambient = vec3(0.05, 0.07, 0.12);
+
 void main() {
     vec4 gb0 = texture2D(u_gbufs[0], v_uv);
     vec4 gb1 = texture2D(u_gbufs[1], v_uv);
@@ -18,10 +20,12 @@ void main() {
     float depth = texture2D(u_depth, v_uv).x;
     // TODO: Extract needed properties from the g-buffers into local variables
 
-    if (depth == 1.0) {
-        gl_FragColor = vec4(0, 0, 0, 0); // set alpha to 0
-        return;
-    }
+    vec3 colmap = gb2.rgb;
 
-    gl_FragColor = vec4(0.1, 0.1, 0.1, 1);  // TODO: replace this
+      if (depth == 1.0) {
+          gl_FragColor = vec4(0, 0, 0, 0); // set alpha to 0
+          return;
+      }
+
+      gl_FragColor = vec4(ambient * colmap, 1.0);
 }
