7ef1ca2911dde3a7e64c5607ad039b765c21055b
Austin Eng <213reeses@gmail.com>
2016-11-03 18:14:25 -0400

    correct gaussian weights

--- glsl/post/blur.frag.glsl
+++ glsl/post/blur.frag.glsl
@@ -10,21 +10,21 @@ uniform vec2 u_dir;
 varying vec2 v_uv;
 
 void main() {
-    // https://github.com/mattdesl/lwjgl-basics/wiki/ShaderLesson5
+    // http://dev.theomader.com/gaussian-kernel-calculator/
     vec4 color = vec4(0.0);
 
     // grab nine texels in the direction of blur
-    color += texture2D(u_color, v_uv - 4.0*u_amount*u_dir/u_resolution) * 0.0162162162;
-    color += texture2D(u_color, v_uv - 3.0*u_amount*u_dir/u_resolution) * 0.0540540541;
-    color += texture2D(u_color, v_uv - 2.0*u_amount*u_dir/u_resolution) * 0.1216216216;
-    color += texture2D(u_color, v_uv - 1.0*u_amount*u_dir/u_resolution) * 0.1945945946;
+    color += texture2D(u_color, v_uv - 4.0*u_amount*u_dir/u_resolution) * 0.000229;
+    color += texture2D(u_color, v_uv - 3.0*u_amount*u_dir/u_resolution) * 0.005977;
+    color += texture2D(u_color, v_uv - 2.0*u_amount*u_dir/u_resolution) * 0.060598;
+    color += texture2D(u_color, v_uv - 1.0*u_amount*u_dir/u_resolution) * 0.241732;
 
-    color += texture2D(u_color, v_uv) * 0.2270270270;
+    color += texture2D(u_color, v_uv) * 0.382928;
 
-    color += texture2D(u_color, v_uv + 1.0*u_amount*u_dir/u_resolution) * 0.1945945946;
-    color += texture2D(u_color, v_uv + 2.0*u_amount*u_dir/u_resolution) * 0.1216216216;
-    color += texture2D(u_color, v_uv + 3.0*u_amount*u_dir/u_resolution) * 0.0540540541;
-    color += texture2D(u_color, v_uv + 4.0*u_amount*u_dir/u_resolution) * 0.0162162162;
+    color += texture2D(u_color, v_uv + 1.0*u_amount*u_dir/u_resolution) * 0.241732;
+    color += texture2D(u_color, v_uv + 2.0*u_amount*u_dir/u_resolution) * 0.060598;
+    color += texture2D(u_color, v_uv + 3.0*u_amount*u_dir/u_resolution) * 0.005977;
+    color += texture2D(u_color, v_uv + 4.0*u_amount*u_dir/u_resolution) * 0.000229;
 
     gl_FragColor = color;
 }
