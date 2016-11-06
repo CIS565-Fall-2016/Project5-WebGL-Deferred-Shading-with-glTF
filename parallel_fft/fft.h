b473e221a2fa94c08186fa74bcf7089b2c0d2e2b
Gabriel Naghi <gabenaghi@hntvlan569.2549.wlan.wireless-resnet.upenn.edu>
2016-11-06 13:20:21 -0500

    checkpoint

--- parallel_fft/fft.h
+++ parallel_fft/fft.h
@@ -9,3 +9,12 @@
 #define M_PI 3.14159265358979323846
 
 void parallel_fft (int N, thrust::complex<double> * samples, thrust::complex<double> * transform);
+
+void checkpoint(const char * print_me, int N, thrust::complex * buf)
+{
+	printf(print_me);
+
+	for(int i = 0; i < N; ++i)
+		printf("%f + i %f\n", buf[i].real, buf[i].imag );
+
+}
