14d7e34cd16ee225938d1e4720c721dea6d987cb
Xueyin <xueyinw@seas.upenn.edu>
2016-11-08 14:17:27 -0500

    update

--- glsl/post/bloomblur.frag.glsl
+++ glsl/post/bloomblur.frag.glsl
@@ -11,9 +11,8 @@ const vec4 SKY_COLOR = vec4(0.01, 0.14, 0.42, 1.0);
 
 void main() {
   vec2 per_pix = vec2(1.0,1.0) / u_texture;
-	vec4 gauss = vec4(10.0,8.0,5.0,2.0);
-  gauss *= (1.0 / 25.0);
-  vec4 color = vec4 (0.0,0.0,0.0,0.0);
+	vec4 gauss = vec4(0.4, 0.32, 0.2, 0.08);
+  vec4 color = vec4 (0.0, 0.0, 0.0, 0.0);
   color += texture2D(u_color, v_uv ) * gauss[0];
   for(int i = 1; i <= 3; i++) {
      color += texture2D(u_color, v_uv + per_pix * vec2(-1.0 * float(i), 0.0)) * gauss[i];    //extract the position's color
--- glsl/post/bloomblurtwice.frag.glsl
+++ glsl/post/bloomblurtwice.frag.glsl
@@ -13,16 +13,14 @@ const vec4 SKY_COLOR = vec4(0.01, 0.14, 0.42, 1.0);
 //http://prideout.net/archive/bloom/
 void main() {
   vec2 per_pix = vec2(1.0,1.0) / u_texture;
-	vec4 gauss = vec4(10.0,8.0,5.0,2.0);
-  gauss *= (1.0 / 25.0);
-  vec4 color = vec4 (0.0,0.0,0.0,0.0);
+  vec4 gauss = vec4(0.4, 0.32, 0.2, 0.08);
+  vec4 color = vec4 (0.0, 0.0, 0.0, 0.0);
   color += texture2D(u_color, v_uv + per_pix * vec2(0, 0)) * gauss[0];
   for(int i = 1; i <= 3; i++) {
      color += texture2D(u_color, v_uv + per_pix * vec2(0, -1 * i)) * gauss[i];    //extract the position's color
      color += texture2D(u_color, v_uv + per_pix * vec2(0, i)) * gauss[i];
   }
   gl_FragColor = color;
-
   vec4 originalcolor = texture2D(u_originalcolor, v_uv);
 // refer to one.frag.glsl
   // if (color.a == 0.0) {
