9f4a7a82cf59680ea7a0c4da9e50abb145a17098
Ethan <ethanabrooks@gmail.com>
2016-11-08 17:42:39 -0500

    debugging with shrek

--- glsl/deferred/blinnphong-pointlight.frag.glsl
+++ glsl/deferred/blinnphong-pointlight.frag.glsl
@@ -46,34 +46,6 @@ void main() {
     if (u_toon == 1) {
         float cuts = float(NUM_CEL_CUTS);
         shading = ROUND(shading * cuts) / cuts;
-
-        mat2 gx = mat2(
-             1,  0,
-             0, -1
-        );
-
-        mat2 gy = mat2(
-            0, -1,
-            1,  0
-        );
-
-        float gx_conv = PRODUCT_AT_OFFSET(0, 0, gx) +
-                       PRODUCT_AT_OFFSET(0, 1, gx) +
-
-                       PRODUCT_AT_OFFSET(1, 0, gx) +
-                       PRODUCT_AT_OFFSET(1, 1, gx) ;
-
-        float gy_conv = PRODUCT_AT_OFFSET(0, 0, gy) +
-                       PRODUCT_AT_OFFSET(0, 1, gy) +
-
-                       PRODUCT_AT_OFFSET(1, 0, gy) +
-                       PRODUCT_AT_OFFSET(1, 1, gy) ;
-
-
-        if (gx_conv + gy_conv > 0.0) {
-           gl_FragColor = vec4(1,0, 0, 0);
-           return;
-        }
     }
 
 
--- glsl/deferred/edges.frag.glsl
+++ glsl/deferred/edges.frag.glsl
@@ -6,17 +6,40 @@ precision highp int;
 #define NUM_CEL_CUTS 7
 #define gb(i) (texture2D(u_gbufs[i], v_uv).xyz)
 #define round(n) (floor((n) + 0.5))
+#define color_at_offset(i, j) (texture2D(u_gbufs[2], v_uv + vec2(i, j)))
+#define PRODUCT_AT_OFFSET(i, j, kernel) (dot(vec4(1), color_at_offset(i, j) * float(kernel[i][j])))
 
-uniform vec3 u_lightCol;
-uniform vec3 u_lightPos;
-uniform vec3 u_camPos;
-uniform float u_lightRad;
 uniform sampler2D u_gbufs[NUM_GBUFFERS];
-uniform sampler2D u_depth;
-uniform int u_toon;
 
 varying vec2 v_uv;
 
 void main() {
 
+    mat2 gx = mat2(
+         1,  0,
+         0, -1
+    );
+
+    mat2 gy = mat2(
+        0, -1,
+        1,  0
+    );
+
+    float gx_conv = PRODUCT_AT_OFFSET(0, 0, gx) +
+                    PRODUCT_AT_OFFSET(0, 1, gx) +
+
+                    PRODUCT_AT_OFFSET(1, 0, gx) +
+                    PRODUCT_AT_OFFSET(1, 1, gx) ;
+
+    float gy_conv = PRODUCT_AT_OFFSET(0, 0, gy) +
+                    PRODUCT_AT_OFFSET(0, 1, gy) +
+
+                    PRODUCT_AT_OFFSET(1, 0, gy) +
+                    PRODUCT_AT_OFFSET(1, 1, gy) ;
+
+    if (gx_conv + gy_conv > -1000.0) {
+       gl_FragColor = vec4(1, 0, 0, 0);
+    }
+
+   gl_FragColor = vec4(1, 0, 0, 0);
 }
